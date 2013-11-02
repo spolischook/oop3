2fb5714937f0b6b6a695883513f8d6a5c4102afa
Sergey Polischuk <spolischook@gmail.com>
2013-11-02 23:25:38 +0200

    Use symfony/http-kernel

--- index.php
+++ index.php
@@ -2,25 +2,33 @@
 
 require_once 'vendor/autoload.php';
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Route;
+use Symfony\Component\HttpFoundation\Request,
+    Symfony\Component\EventDispatcher\EventDispatcher,
+    Symfony\Component\HttpKernel\EventListener\RouterListener,
+    Symfony\Component\HttpKernel\Controller\ControllerResolver,
+    Symfony\Component\HttpKernel\HttpKernel,
+    Symfony\Component\Routing\Matcher\UrlMatcher,
+    Symfony\Component\Routing\RequestContext,
+    Symfony\Component\Routing\RouteCollection,
+    Symfony\Component\Routing\Route;
 
 $routes = new RouteCollection();
-$routes->add('index', new Route('/', array('controller' => 'Spolischook\Controller\MainController', 'action' => 'indexAction')));
-$routes->add('foo', new Route('/foo', array('controller' => 'Spolischook\Controller\MainController', 'action' => 'fooAction')));
-$routes->add('greeting', new Route('/hello', array('controller' => 'Spolischook\Controller\MainController', 'action' => 'helloAction')));
+$routes->add('index', new Route('/', array('_controller' => 'Spolischook\Controller\MainController::indexAction')));
+$routes->add('foo', new Route('/foo', array('_controller' => 'Spolischook\Controller\MainController::fooAction')));
+$routes->add('greeting', new Route('/hello/{name}', array('_controller' => 'Spolischook\Controller\MainController::helloAction')));
 
 $request = Request::createFromGlobals();
+
 $context = new RequestContext();
 $context->fromRequest($request);
+
 $matcher = new UrlMatcher($routes, $context);
 
-$parameters = $matcher->match($context->getPathInfo());
-$content = call_user_func_array(array(new $parameters['controller'], $parameters['action']), array($request));
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new RouterListener($matcher));
 
-$response = new Response($content);
-$response->send();
+$resolver = new ControllerResolver();
+
+$kernel = new HttpKernel($dispatcher, $resolver);
+
+$kernel->handle($request)->send();
