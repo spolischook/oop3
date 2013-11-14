7286298bd881a8b54ddca1484ee6285182e6e0ea
Sergey Polischuk <spolischook@gmail.com>
2013-11-14 05:37:52 +0200

    Update index.php, inject entity manager into controller

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ use Symfony\Component\HttpFoundation\Request,
     Symfony\Component\Routing\Matcher\UrlMatcher,
     Symfony\Component\Routing\RequestContext,
     Symfony\Component\Routing\RouteCollection,
-    Symfony\Component\Routing\Route;
+    Symfony\Component\Routing\Route,
+    Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Bridge\Twig\Extension\RoutingExtension;
 use Assetic\Factory\AssetFactory,
     Assetic\Factory\Worker\CacheBustingWorker,
     Assetic\Filter\Yui\CssCompressorFilter,
@@ -27,15 +29,22 @@ use Assetic\Factory\AssetFactory,
     Assetic\Asset\GlobAsset;
 use Symfony\Component\Yaml\Yaml;
 
+require_once 'config/bootstrap.php';
+
+$em = DoctrineBootstrap::getEntityManager();
+
 $routes = new RouteCollection();
 $routes->add('index', new Route('/', array('_controller' => 'Spolischook\Controller\MainController::indexAction')));
 $routes->add('foo', new Route('/foo', array('_controller' => 'Spolischook\Controller\MainController::fooAction')));
 $routes->add('greeting', new Route('/hello/{name}', array('_controller' => 'Spolischook\Controller\MainController::helloAction')));
+$routes->add('load_fixtures', new Route('/load-fixtures', array('_controller' => 'Spolischook\Controller\MainController::loadFixturesAction')));
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/view');
 $twig = new Twig_Environment($loader, array(
-    'cache' => __DIR__ . '/view/cache',
+//    'cache' => __DIR__ . '/view/cache',
 ));
+
+
 $twig->addExtension(new AsseticExtension(new AssetFactory(__DIR__ . '/public')));
 
 $assetManager = new AssetManager();
@@ -52,6 +61,7 @@ $js = new AssetCollection(array(
     new FileAsset(__DIR__ . '/vendor/components/jquery/jquery.js'),
     new FileAsset(__DIR__ . '/vendor/twbs/bootstrap/dist/js/bootstrap.min.js'),
     new FileAsset(__DIR__ . '/vendor/twbs/bootstrap/docs-assets/js/holder.js'),
+    new FileAsset(__DIR__ . '/public/load-fixtures.js'),
 ), array(
     new JsCompressorFilter(__DIR__ . '/public/yuicompressor-2.4.8.jar'),
 ));
@@ -71,14 +81,18 @@ $context = new RequestContext();
 $context->fromRequest($request);
 
 $matcher = new UrlMatcher($routes, $context);
+$generator = new UrlGenerator($routes, $context);
+
+$twig->addExtension(new RoutingExtension($generator));
 
 $dispatcher = new EventDispatcher();
 $dispatcher->addSubscriber(new RouterListener($matcher));
 
-$dispatcher->addListener(KernelEvents::CONTROLLER, function (Event $event) use ($twig) {
+$dispatcher->addListener(KernelEvents::CONTROLLER, function (Event $event) use ($twig, $em) {
     $controllerArray = $event->getController();
     $controller = $controllerArray[0];
     $controller->setTwig($twig);
+    $controller->setEntityManager($em);
 });
 
 $resolver = new ControllerResolver();
