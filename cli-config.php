992ff95ae48976186dc14ab0037050bc06b956bb
Sergey Polischuk <spolischook@gmail.com>
2013-11-15 13:37:48 +0200

    Temporary fix major bug at doctrine 2.3.4. See http://www.doctrine-project.org/jira/browse/DDC-2486

--- cli-config.php
+++ cli-config.php
@@ -4,8 +4,11 @@ require_once '../vendor/autoload.php';
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 
-require_once 'bootstrap.php';
+require_once 'config/bootstrap.php';
 
 $entityManager = DoctrineBootstrap::getEntityManager();
 
-return ConsoleRunner::createHelperSet($entityManager);
+return $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($entityManager->getConnection()),
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
+));
